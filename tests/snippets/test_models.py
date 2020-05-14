05b2a9c1146c8b88d879bb4b4ef3644203e1fd46
spkuehl <skuehlhorn@gmail.com>
2020-05-13 20:18:08 -0500

    Setup unit test file system and write first model unit test.

---
+++ tests/snippets/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from snippets.models import Snippet
+from django.contrib.auth.models import User
+
+
+class SnippetTest(TestCase):
+    """ Test module for Snippet model """
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            id=1,username='jacob', email='jacob@…', password='top_secret')
+
+        Snippet.objects.create(
+            id=1,
+            created='2018-01-04T18:09:36.612Z',
+            title='serializers.py',
+            code='test code field',
+            linenos=False,
+            language='python',
+            style='default',
+            owner=self.user,
+            highlighted='test highlighted field'
+            )
+
+    def test_string_representation(self):
+        snip = Snippet.objects.get(id=1)
+        self.assertEqual(str(snip), snip.title)
---
+++ tests/snippets/test_views.py
@@ -0,0 +1,3 @@
+import pytest
+from django.urls import reverse
+from django.conf import settings
