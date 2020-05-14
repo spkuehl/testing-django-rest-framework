5c6b5876bf611119bd8852d1a81ec8374d38fccf
spkuehl <skuehlhorn@gmail.com>
2020-05-13 21:37:05 -0500

    Add unit tests for user and snippet viewsets to test empty lists.

--- tests/snippets/test_views.py
+++ tests/snippets/test_views.py
@@ -1,3 +1,19 @@
 import pytest
 from django.urls import reverse
 from django.conf import settings
+
+
+@pytest.mark.django_db
+def test_fetch_users_returns_emply_list(client):
+    """ Make sure that we get valid JSON response if we have no users created """
+    response = client.get(reverse('user-list'))
+    data = response.json()
+    assert 0 == len(data["results"])
+
+
+@pytest.mark.django_db
+def test_fetch_snippets_returns_emply_list(client):
+    """ Make sure that we get valid JSON response if we have no snippets created """
+    response = client.get(reverse('snippet-list'))
+    data = response.json()
+    assert 0 == len(data["results"])
