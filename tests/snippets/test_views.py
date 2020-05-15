abdb579e70fbdf37ef6d838faa39327ac0f2d49e
spkuehl <skuehlhorn@gmail.com>
2020-05-14 20:50:14 -0500

    Add api endpoint POST test create snippet.

--- tests/snippets/test_views.py
+++ tests/snippets/test_views.py
@@ -1,6 +1,9 @@
 import pytest
 from django.urls import reverse
 from django.conf import settings
+from rest_framework.test import APIClient, APIRequestFactory
+from snippets.models import Snippet
+from django.contrib.auth.models import User
 
 
 @pytest.mark.django_db
@@ -17,3 +20,23 @@ def test_fetch_snippets_returns_emply_list(client):
     response = client.get(reverse('snippet-list'))
     data = response.json()
     assert 0 == len(data["results"])
+
+
+@pytest.mark.django_db
+def test_api_create_snippet(client):
+    client = APIClient()
+    user = User.objects.create_user('john',
+                                    'lennon@thebeatles.com', 'johnpassword')
+    client.login(username='john', password='johnpassword')
+    request = client.post('/snippets/',{
+        "url": "",
+        "id": 1,
+        "highlight": "",
+        "owner": "john",
+        "title": "rolling knolls",
+        "code": "123123k",
+        "linenos": 'true',
+        "language": "abap",
+        "style": "abap"
+        }, format='json')
+    assert Snippet.objects.count() == 1
